5f1c653a5fbac89d9c4fccde3f07564940676742
Michael Yeung <yeungm95@gmail.com>
2020-02-25 02:24:04 -0800

    add questions 4 and 5; add addClass methods to highlight the logo of whatever language that is the result

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
     var question3 = $("#question3").val();
     var question4 = $("#question4").val();
     var question5 = $("#question5").val();
+    var question5 = $("#question6").val();
+    var question5 = $("#question7").val();
+    var question5 = $("#question8").val();
 
     var rubyScore = 0
     var csharpScore = 0
     var javascriptScore = 0
     var pythonScore = 0
 
-
+    // How the score for each language is tallied. Each question has it's own if/else logic.
     if (question1 === "pythonAndRuby") {
       pythonScore = pythonScore + 1
       rubyScore = rubyScore + 1 
@@ -36,28 +39,57 @@ $(document).ready(function() {
       pythonScore = pythonScore + 1
     }
 
+    if (question4 === "python") {
+      pythonScore = pythonScore + 1
+    } else if (question4 === "rubyAndJavascript") {
+      rubyScore = rubyScore + 1
+      javascriptScore = javascriptScore + 1
+    } else if (question4 === "csharp") {
+      csharpScore = csharpScore + 1
+    }
 
+    if (question5 === "csharp") {
+      csharpScore = csharpScore + 1
+    } else if (question5 === "ruby") {
+      rubyScore = rubyScore + 1
+    } else if (question5 === "python") {
+      pythonScore = pythonScore + 1
+    } else if (question5 === "javascript") {
+      javascriptScore = javascriptScore + 1
+    }
     
+    // Used to check final score
+    console.log(rubyScore, csharpScore, javascriptScore, pythonScore) 
+
+    // The result is whatever language has the highest score. The resulting logo is then highlighted and the rest of the logos are made opaque.
     if (rubyScore > csharpScore && rubyScore > javascriptScore && rubyScore > pythonScore) {
       $("#preferRuby").show()
       $("#preferCsharp").hide()
       $("#preferJavascript").hide()
       $("#preferPython").hide()
+      $(".logos :not(#rubylogo)").addClass("dehighlight")
+      $(".logos #rubylogo").addClass("highlight")
     } else if (csharpScore > rubyScore && csharpScore > javascriptScore && csharpScore > pythonScore) {
       $("#preferCsharp").show()
       $("#preferJavascript").hide()
       $("#preferPython").hide()
       $("#preferRuby").hide()
+      $(".logos :not(#csharplogo)").addClass("dehighlight")
+      $(".logos #csharplogo").addClass("highlight")
     } else if (javascriptScore > rubyScore && javascriptScore > csharpScore && javascriptScore > pythonScore) {
       $("#preferJavascript").show()
       $("#preferPython").hide()
       $("#preferRuby").hide()
       $("#preferCsharp").hide()
+      $(".logos :not(#javascriptlogo)").addClass("dehighlight")
+      $(".logos #javascriptlogo").addClass("highlight")
     } else if (pythonScore > rubyScore && pythonScore > csharpScore && pythonScore > javascriptScore) {
       $("#preferPython").show()
       $("#preferCsharp").hide()
       $("#preferJavascript").hide()
       $("#preferRuby").hide()
+      $(".logos :not(#pythonlogo)").addClass("dehighlight")
+      $(".logos #pythonlogo").addClass("highlight")
     }
   })
 })
